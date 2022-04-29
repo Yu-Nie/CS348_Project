50abeed86cb851248c6928c49b011e038b0bac87
kxiong2001 <kxiong2001@hotmail.com>
2022-04-29 16:48:44 -0400

    Revert "login page"
    This reverts commit a12903e19c549a7421ba18240dd5c107cd48ecc1.

--- boiler_delivery_backend/api/urls.py
+++ boiler_delivery_backend/api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, re_path, include
+from django.urls import path, re_path
 from django.contrib import admin
 from .views import *
 
@@ -6,13 +6,12 @@ from .views import *
 urlpatterns = [
     path('', mainPageView),
     path('customer', customerView, name = 'customer'),
-    #path('userlogin/', customerLoginView),
+    path('userlogin/', customerLoginView),
     path('usersignup/', customerSignupView),
     path('food/', getMenusView),
     path('restaurant/', restaurantView),
     path('restaurant/login', restaurantView),
     path('restaurant/addRestaurant', addRestaurantView),
     path('admin/', admin.site.urls),
-    path('', include("django.contrib.auth.urls")),
 
 ]
