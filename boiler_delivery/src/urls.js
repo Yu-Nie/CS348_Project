036f7992c6909fd76c6ac15be2e4ec7d85569f71
kxiong2001 <kxiong2001@hotmail.com>
2022-04-29 01:09:50 -0400

    removed frontend folder to save time

--- boiler_delivery/src/urls.js
+++
@@ -1,9 +0,0 @@
-const base_url = "http://127.0.0.1:8000/";
-
-export default {
-    base_url: base_url,
-    login: base_url + "login/",
-    signup: base_url + "signup/",
-    restaurants: base_url + "restaurants/",
-    food: base_url + "food/",
-};
